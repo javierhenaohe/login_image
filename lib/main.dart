094f2965f35ccd5d5dc94fb4c3eb4bd3f2ad9f00
Javier Henao Hernandez <javier.henao@outlook.com>
2022-11-14 18:06:57 -0500

    inicio v205 segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_image/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,15 +10,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'Login PTM',
+      initialRoute: 'login',
+      routes: {
+        'login':(_) => LoginScreen(),
+        'home':(_) => HomeScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:login_image/screens/home_screen.dart';
+export 'package:login_image/screens/login_screen.dart';
+
+
