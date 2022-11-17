63697be9585ee0798e17785405ed6b607ec578df
Javier Henao Hernandez <javier.henao@outlook.com>
2022-11-17 09:35:12 -0500

    inicio v221 13 commit

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -14,7 +14,20 @@ class ProductCard extends StatelessWidget {
           alignment: Alignment.bottomLeft,
           children: [
             _BackgroundImage(),
-            _producDetails(), //se ve la imagen de fondo
+            _ProducDetails(), //se ve la imagen de fondo
+            Positioned(
+              top: 0,
+              right: 0,
+              child: _PriceTag(), // muestra datos en esquina superior derecha
+            ),
+
+            //TODO: mostrar de manera condicional
+            Positioned(
+              top: 0,
+              left: 0,
+              child:
+                  _NotAvailable(), // muestra datos en esquina superior izquierda
+            )
           ],
         ),
       ),
@@ -34,17 +47,110 @@ class ProductCard extends StatelessWidget {
       );
 }
 
-class _producDetails extends StatelessWidget {
-  //const _producDetails({Key? key,}) : super(key: key);
+class _NotAvailable extends StatelessWidget {
+  //const _NotAvailable({Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      alignment: Alignment.center,
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'Sin Aviso',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
       height: 70,
-      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.yellow[800],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          bottomRight: Radius.circular(25),
+        ),
+      ),
     );
   }
+}
+
+class _PriceTag extends StatelessWidget {
+  //const _PirceTag({Key? key,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: FittedBox(
+        // permite adaptar el texto en la caja superior derecha
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            '\PPTMXX00', //'\$PPTMXXXX',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
+        ),
+      ),
+    );
+  }
+}
+
+class _ProducDetails extends StatelessWidget {
+  //const _producDetails({Key? key,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        height: 70,
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Descripcion del aviso o falla',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              'ID disco duro',
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(25), bottomLeft: Radius.circular(25)),
+      );
 }
 
 class _BackgroundImage extends StatelessWidget {
